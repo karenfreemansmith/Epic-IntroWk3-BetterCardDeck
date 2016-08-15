c92ec94e7afe2d42460b0c4062dc5069cc7d3117
Angie Smith and Karen Freeman-Smith <karen@>
2016-08-15 14:16:48 -0700

    print out a list of cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+
+var values = ["ace",2,3,4,5,6,7,8,9,10,"jack", "queen", "king"];
+var suits = ["diamonds", "hearts", "clubs", "spades"];
+
+suits.forEach(function(suit) {
+  values.forEach(function(value) {
+    $("ul").append("<li>" + value + " of " + suit +"</li>");
+  });
+});
