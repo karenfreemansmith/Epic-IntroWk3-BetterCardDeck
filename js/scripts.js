4922a6a2b740b77c0f84689c042fc06fb24ba97b
Angie Smith and Karen Freeman-Smith <karen@>
2016-08-15 17:29:46 -0700

    replace text with images for cards, proof of concept, needs more card images

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 
-var values = ["ace",2,3,4,5,6,7,8,9,10,"jack", "queen", "king"];
+var values = ["A",2,3,4,5,6,7,8,9,10,"J", "Q", "K"];
 var suits = ["diamonds", "hearts", "clubs", "spades"];
 
 suits.forEach(function(suit) {
+  $("div.cards").append("<div class='"+suit+" row'></div>")
   values.forEach(function(value) {
-    $("ul").append("<li>" + value + " of " + suit +"</li>");
+    $("div."+suit).append("<img src='img/" + value + suit + ".jpg' alt='"+value+" of " + suit + "'>");
   });
 });
